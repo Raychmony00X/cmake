954545ee80fd621950a17b862f8c63572859eef9
Raychmony00X <shkunov.artyom@mail.ru>
2026-05-03 11:04:00 +0300

    Add files via upload

---
+++ simple-project/SimpleProject.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::string userName;
+	std::cout << "Введите имя: ";
+	std::cin >> userName;
+	std::cout << "Здравствуйте, " << userName << "!" << std::endl;
+
+
+
+	return 0;
+}
